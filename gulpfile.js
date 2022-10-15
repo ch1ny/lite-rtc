9ddeb5d11c94526588f6a5441a6b5a8293729b5c
德布罗煜 <1056317718@qq.com>
2022-10-15 14:15:50 +0800

    fix: 外挂打包umd

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const clean = require('gulp-clean');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const tsify = require('tsify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 const webpack = require('webpack');
 const gulpWebpack = require('webpack-stream');
 
@@ -21,6 +26,43 @@ gulp.task('build', () => {
 			gulpWebpack(
 				{
 					output: {
+						filename: 'index.js',
+						library: {
+							type: 'umd',
+							name: 'LiteRTC',
+						},
+					},
+					mode: 'production',
+				},
+				webpack
+			)
+		)
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', () => {
+	return browserify({
+		basedir: '.',
+		debug: true,
+		entries: ['src/index.ts'],
+		cache: {},
+		packageCache: {},
+	})
+		.plugin(tsify)
+		.bundle()
+		.pipe(source('index.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-umd', () => {
+	return gulp
+		.src('dist/index.js')
+		.pipe(
+			gulpWebpack(
+				{
+					output: {
 						filename: 'index.umd.js',
 						library: {
 							type: 'umd',
@@ -35,8 +77,8 @@ gulp.task('build', () => {
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean-not-umd-js', function () {
-	return gulp.src(['dist/**/*.js', '!dist/**/*.umd.js'], { read: false }).pipe(clean());
+gulp.task('clean-js', function () {
+	return gulp.src(['dist/**/*.js', '!dist/index.js'], { read: false }).pipe(clean());
 });
 
 gulp.task(
@@ -44,7 +86,8 @@ gulp.task(
 	gulp.series(
 		gulp.parallel('clean'),
 		gulp.parallel('tsc'),
+		gulp.parallel('clean-js'),
 		gulp.parallel('build'),
-		gulp.parallel('clean-not-umd-js')
+		gulp.parallel('build-umd')
 	)
 );
