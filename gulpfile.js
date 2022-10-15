cfb0c1d54780954c6b20db135f023782f7b0641c
德布罗煜 <1056317718@qq.com>
2022-10-15 13:15:06 +0800

    feat: 修改构建为umd包

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const clean = require('gulp-clean');
-const uglify = require('gulp-uglify');
-const browserify = require('browserify');
-const tsify = require('tsify');
-const buffer = require('vinyl-buffer');
-const source = require('vinyl-source-stream');
+const webpack = require('webpack');
+const gulpWebpack = require('webpack-stream');
 
 const tsProject = ts.createProject('tsconfig.json');
 
@@ -17,24 +14,29 @@ gulp.task('tsc', function () {
 	return tsProject.src().pipe(tsProject()).pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean-js', function () {
-	return gulp.src('dist/**/*.js', { read: false }).pipe(clean('*.js'));
+gulp.task('build', () => {
+	return gulp
+		.src('dist/index.js')
+		.pipe(
+			gulpWebpack(
+				{
+					output: {
+						filename: 'index.umd.js',
+						library: {
+							type: 'umd',
+							name: 'LiteRTC',
+						},
+					},
+					mode: 'production',
+				},
+				webpack
+			)
+		)
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', () => {
-	return browserify({
-		basedir: '.',
-		debug: true,
-		entries: ['src/index.ts'],
-		cache: {},
-		packageCache: {},
-	})
-		.plugin(tsify)
-		.bundle()
-		.pipe(source('index.js'))
-		.pipe(buffer())
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+gulp.task('clean-not-umd-js', function () {
+	return gulp.src(['dist/**/*.js', '!dist/**/*.umd.js'], { read: false }).pipe(clean());
 });
 
 gulp.task(
@@ -42,7 +44,7 @@ gulp.task(
 	gulp.series(
 		gulp.parallel('clean'),
 		gulp.parallel('tsc'),
-		gulp.parallel('clean-js'),
-		gulp.parallel('build')
+		gulp.parallel('build'),
+		gulp.parallel('clean-not-umd-js')
 	)
 );
